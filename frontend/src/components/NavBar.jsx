b5632d85e7f81299b15250ab54940177c56ccff3
HangoverHGV <hangobogdan@gmail.com>
2025-02-06 15:01:35 +0200

    Data and card

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,20 +3,23 @@ import {useState, useEffect} from "react";
 import "./styles/NavBar.css";
 
 
-export default function Navbar(){
-      return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  );
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">Blog</a>
+                </li>
+                <li>
+                    <a href="/login">Login</a>
+                </li>
+                <li>
+                    <a href="/signup">Signup</a>
+                </li>
+            </ul>
+        </nav>
+    );
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Navbar from "../components/NavBar";
+import Card from "../components/Card";
+import data from "../data/data.json";
 
 export default function HomePage() {
+
+
     return <div className="container">
         <Navbar/>
-        <h1>Home Page </h1>
-        <p>Welcome to the home page.</p>
+        <Card data={data.aboutme}/>
     </div>
 }
 
