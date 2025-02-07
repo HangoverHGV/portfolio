0e9177095eabf2a41e028aa44a691589cc2fe948
HangoverHGV <hangobogdan@gmail.com>
2025-02-07 16:24:16 +0200

    Create Post

--- frontend/src/components/RichTextEditor.jsx
+++ frontend/src/components/RichTextEditor.jsx
@@ -1,41 +1,52 @@
 import React, { useEffect, useRef } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import styles from "./styles/RichTextEditor.module.css";
+        import ReactQuill from "react-quill";
+        import "react-quill/dist/quill.snow.css";
+        import ImageResize from "quill-image-resize-module-react";
+        import styles from "./styles/RichTextEditor.module.css";
+        import Quill from "quill";
 
-export default function RichTextEditor({ value, onChange }) {
-    const quillRef = useRef(null);
+        // Register the image resize module
+        Quill.register("modules/imageResize", ImageResize);
 
-    useEffect(() => {
-        if (quillRef.current) {
-            const editor = quillRef.current.getEditor();
-            if (editor.root.innerHTML !== value) {
-                editor.clipboard.dangerouslyPasteHTML(value);
-            }
+        export default function RichTextEditor({ value, onChange }) {
+            const quillRef = useRef(null);
+
+            useEffect(() => {
+                if (quillRef.current) {
+                    const editor = quillRef.current.getEditor();
+                    if (editor.root.innerHTML !== value) {
+                        editor.clipboard.dangerouslyPasteHTML(value);
+                    }
+                }
+            }, [value]);
+
+            const modules = {
+                toolbar: [
+                    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+                    ['bold', 'italic', 'underline'],
+                    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                    [{ 'align': [] }],
+                    ['link', 'image'],
+                    ['clean']
+                ],
+                imageResize: {
+                    parchment: Quill.import('parchment'),
+                    modules: ['Resize', 'DisplaySize', 'Toolbar']
+                }
+            };
+
+            return (
+                <div>
+                    <div className={styles.editorContainer}>
+                        <ReactQuill
+                            ref={quillRef}
+                            theme="snow"
+                            value={value}
+                            onChange={onChange}
+                            modules={modules}
+                            className={styles.editor}
+                        />
+                    </div>
+                </div>
+            );
         }
-    }, [value]);
-        const modules = {
-        toolbar: [
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            ['bold', 'italic', 'underline'],
-            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-            [{ 'align': [] }],
-            ['link', 'image'],
-            ['clean']
-        ]
-    };
-    return (
-        <div>
-            <div className={styles.editorContainer}>
-                <ReactQuill
-                    ref={quillRef}
-                    theme="snow"
-                    value={value}
-                    onChange={onChange}
-                    modules={modules}
-                    className={styles.editor}
-                />
-            </div>
-        </div>
-    );
-}
