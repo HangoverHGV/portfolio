d0f6400d98bee93d8d7d81ab2c892881a8d27ccf
HangoverHGV <hangobogdan@gmail.com>
2025-02-14 10:58:20 +0200

    Deployment configs

--- backend/configs.py
+++ backend/configs.py
@@ -48,6 +48,8 @@ origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://127.0.0.1:3000"
+    "http://hangobogdan.com",
+    "https://hangobogdan.com",
 ]
 
 app.add_middleware(
